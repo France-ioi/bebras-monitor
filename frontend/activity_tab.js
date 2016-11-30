c7da17c1b23f58bf283927db58050066dae7fc3a
Sebastien Carlier <s.carlier@epixode.fr>
2016-11-30 17:31:02 +0000

    activity tab: allow selecting entries, display details

--- frontend/activity_tab.js
+++ frontend/activity_tab.js
@@ -2,22 +2,41 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
 import EpicComponent from 'epic-component';
-import {use, defineSelector, defineView} from 'epic-linker';
+import classnames from 'classnames';
+import {defineAction, defineSelector, defineView, addReducer} from 'epic-linker';
 
 export default function* (deps) {
 
+  yield defineAction('selectEntry', 'ActivityTab.Entry.Select');
+
+  yield addReducer('selectEntry', function (state, action) {
+    return {...state, selectedEntryKey: action.key};
+  });
+
   yield defineSelector('ActivityTabSelector', function (state, props) {
-    const {topEntries} = state;
-    return {topEntries};
+    const {topEntries, selectedEntryKey} = state;
+    let selectedEntry;
+    const nEntries = topEntries.length;
+    for (let i = 0; i < nEntries; i += 1) {
+      let entry = topEntries[i];
+      if (entry.key === selectedEntryKey) {
+        selectedEntry = entry;
+      }
+    }
+    return {topEntries, selectedEntry};
   });
 
   yield defineView('ActivityTab', 'ActivityTabSelector', EpicComponent(self => {
 
-    const renderEntry = function (entry) {
+    const onSelectEntry = function (event) {
+      const key = event.currentTarget.getAttribute('data-key');
+      self.props.dispatch({type: deps.selectEntry, key});
+    };
+
+    const renderEntry = function (entry, isSelected) {
       const {key, ip, total, domains} = entry;
       return (
-        <div className="entry-table" key={key}>
-          <span className="entry-key">{key}</span>
+        <div className={classnames(["entry-table", isSelected && "entry-selected"])} key={key} onClick={onSelectEntry} data-key={key}>
           <span className="entry-ip number">{ip}</span>
           <span className="entry-total number">{total}</span>
           <span className="entry-domain number">
@@ -29,11 +48,17 @@ export default function* (deps) {
     };
 
     self.render = function () {
-      const {topEntries} = self.props;
+      const {topEntries, selectedEntry} = self.props;
+      // <span className="entry-key">{key}</span>
       return (
         <div className="row">
-          <div className="col-md-12">
-            {topEntries && <div>{topEntries.map(renderEntry)}</div>}
+          <div className="col-md-6">
+            {topEntries && <div>{topEntries.map(entry =>
+              renderEntry(entry, selectedEntry === entry)
+            )}</div>}
+          </div>
+          <div className="col-md-6">
+            <div style={{wordBreak: 'break-word'}}>{JSON.stringify(selectedEntry)}</div>
           </div>
         </div>
       );
